d3d129af7937ea7755cb788d4986d119c1bff7ab
SamuelDiaz <sala@HOR-SALA501-018.unihorizonte.salas>
2019-10-29 15:51:51 -0500

    mi primer commit

---
+++ senasalud/src/senasalud/Senasalud.java
@@ -0,0 +1,17 @@
+
+package senasalud;
+
+import java.util.Scanner;
+
+public class Senasalud {
+    public static void main(String[] args) {
+       String nomb;
+        Scanner nombre = new Scanner (System.in);
+        System.out.println("Digite su nombre");
+        nomb=nombre.next();
+        System.out.println("su nombre es "+nomb);
+        
+        
+    }
+    
+}
